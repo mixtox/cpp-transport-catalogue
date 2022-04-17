f61edf8120593f818e2aeba095620bb7b8bf3184
Aleksei Belozerov <88431789+mixtox@users.noreply.github.com>
2022-04-17 21:57:19 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,14 @@
-// место для вашего кода
+#pragma once
+#include "transport_catalogue.h"
+
+#include <utility>
+#include <string>
+#include <string_view>
+#include <istream>
+#include <iostream>
+
+namespace transport_catalogue::input_reader
+
+{
+    void ProcessInput(TransportCatalogue&, std::istream&);
+}
