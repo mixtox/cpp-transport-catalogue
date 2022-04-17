74ace176eb15d6759c8dd8c345cf8648be2cd10f
Aleksei Belozerov <88431789+mixtox@users.noreply.github.com>
2022-04-17 22:02:46 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,18 @@
-// место для вашего кода
+#include "geo.h"
+#include "input_reader.h"
+#include "transport_catalogue.h"
+#include "stat_reader.h"
+
+#include <sstream>
+
+int main()
+{
+    using namespace std::string_literals;
+
+    transport_catalogue::TransportCatalogue trans_cat_;
+
+    transport_catalogue::input_reader::ProcessInput(trans_cat_, std::cin);
+    transport_catalogue::stat_reader::ProcessRequest(trans_cat_, std::cin);
+
+    return 0;
+}
