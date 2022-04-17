f4a25ea809825b91eafee9c0a7b7f22acd33aab0
Aleksei Belozerov <88431789+mixtox@users.noreply.github.com>
2022-04-17 22:05:54 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,14 @@
-// место для вашего кода
+#pragma once
+#include "transport_catalogue.h"
+
+#include <utility>
+#include <string>
+#include <string_view>
+#include <istream>
+#include <iostream>
+
+namespace transport_catalogue::stat_reader
+
+{
+    void ProcessRequest(TransportCatalogue&, std::istream&);
+}
