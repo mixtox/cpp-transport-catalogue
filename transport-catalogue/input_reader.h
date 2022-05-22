94162866f4888fe1736d843b31ee1d651774dbb3
Aleksei Belozerov <88431789+mixtox@users.noreply.github.com>
2022-05-23 00:40:38 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -6,9 +6,23 @@
 #include <string_view>
 #include <istream>
 #include <iostream>
+#include <vector>
+
+namespace detail
+{
+    std::pair<std::string_view, std::string_view> Split(std::string_view, char, int count = 1);
+    std::string_view DeleteLeftSpaces(std::string_view);
+    std::string_view DeleteRightSpaces(std::string_view);
+    std::string_view TrimString(std::string_view);
+}
 
 namespace transport_catalogue::input_reader
 
 {
     void ProcessInput(TransportCatalogue&, std::istream&);
+    void ProcessInputQueries(TransportCatalogue&, std::vector<InputQuery>&);
+
+    Stop ProcessQueryAddStop(std::string&);
+    void ProcessQueryAddStopsDistance(TransportCatalogue&, std::string&);
+    Bus ProcessQueryAddBus(TransportCatalogue&, std::string&);
 }
