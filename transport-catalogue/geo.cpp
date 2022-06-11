e3e0fceb48ac7d055ac6867fd00c91070e0b7be6
Aleksei Belozerov <88431789+mixtox@users.noreply.github.com>
2022-06-11 19:30:31 +0300

    Update geo.cpp
    10 sprint project

--- transport-catalogue/geo.cpp
+++ transport-catalogue/geo.cpp
@@ -1,16 +1,19 @@
+#define _USE_MATH_DEFINES
 #include "geo.h"
 
-namespace transport_catalogue::geo
-{
+#include <cmath>
 
-    bool transport_catalogue::geo::Coordinates::operator==(const Coordinates& other) const
-    {
-        return lat == other.lat && lng == other.lng;
+namespace geo {
+  
+double ComputeDistance(Coordinates from, Coordinates to) {
+    using namespace std;
+    if (from == to) {
+        return 0;
     }
-
-    bool transport_catalogue::geo::Coordinates::operator!=(const Coordinates& other) const
-    {
-        return !(*this == other);
-    }
-
+    static const double dr = M_PI / 180.;
+    return acos(sin(from.lat * dr) * sin(to.lat * dr)
+                + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
+        * EARTH_RADIUS;
 }
+
+}  // namespace geo
