1b799ef83156f6cf98348995250117ea85c1e216
Aleksei Belozerov <88431789+mixtox@users.noreply.github.com>
2022-06-16 00:10:27 +0300

    11 sprint project

--- transport-catalogue/request_handler.cpp
+++ transport-catalogue/request_handler.cpp
@@ -1,7 +1,7 @@
 #include <sstream>
 #include "request_handler.h"
 
-RequestHandler::RequestHandler(const transport_catalogue::TransportCatalogue &tc, const renderer::MapRenderer &renderer)
+RequestHandler::RequestHandler(const transport_catalogue::TransportCatalogue& tc, const renderer::MapRenderer& renderer)
     : tc_(tc)
     , renderer_(renderer) {}
 
@@ -11,31 +11,45 @@ svg::Document RequestHandler::RenderMap() const {
 
 json::Dict RequestHandler::GetRoute(const json::Node& request) {
     using namespace std::string_literals;
-    json::Dict result;
 
     const auto bus_info = tc_.GetRouteInfo(request.AsMap().at("name"s).AsString());
     int id = request.AsMap().at("id"s).AsInt();
 
-    result["request_id"s] = id;
     if (bus_info.has_value()) {
-        result["curvature"s] = bus_info.value().curvature;
-        result["route_length"s] = bus_info.value().route_length;
-        result["stop_count"] = bus_info.value().stop_count;
-        result["unique_stop_count"] = bus_info.value().unique_stop;
-    } else {
-        result["error_message"s] = "not found"s;
+        return json::Builder{}
+            .StartDict()
+            .Key("request_id"s)
+            .Value(id)
+            .Key("curvature"s)
+            .Value(bus_info.value().curvature)
+            .Key("route_length"s)
+            .Value(bus_info.value().route_length)
+            .Key("stop_count"s)
+            .Value(bus_info.value().stop_count)
+            .Key("unique_stop_count"s)
+            .Value(bus_info.value().unique_stop)
+            .EndDict()
+            .Build()
+            .AsMap();
     }
-
-    return result;
+    else {
+        return json::Builder{}
+            .StartDict()
+            .Key("request_id"s)
+            .Value(id)
+            .Key("error_message"s)
+            .Value("not found"s)
+            .EndDict()
+            .Build()
+            .AsMap();
+    }
 }
 
 json::Dict RequestHandler::GetStop(const json::Node& request) {
     using namespace std::string_literals;
-    json::Dict result;
     auto stop_info = tc_.GetBusesForStop(request.AsMap().at("name"s).AsString());
-
     int id = request.AsMap().at("id"s).AsInt();
-    result["request_id"s] = id;
+
     if (stop_info) {
         std::set<std::string> buses;
         for (auto* bus : *stop_info) {
@@ -47,25 +61,45 @@ json::Dict RequestHandler::GetStop(const json::Node& request) {
             bus_array.push_back(bus);
         }
 
-        result["buses"s] = bus_array;
-
-    } else {
-        result["error_message"s] = "not found"s;
+        return json::Builder{}
+            .StartDict()
+            .Key("request_id"s)
+            .Value(id)
+            .Key("buses"s)
+            .Value(bus_array)
+            .EndDict()
+            .Build()
+            .AsMap();
     }
-    return result;
+    else {
+        return json::Builder{}
+            .StartDict()
+            .Key("request_id"s)
+            .Value(id)
+            .Key("error_message"s)
+            .Value("not found"s)
+            .EndDict()
+            .Build()
+            .AsMap();
+    }
 }
 
 json::Dict RequestHandler::GetMap(const json::Node& request) {
     using namespace std::string_literals;
-    json::Dict result;
-
-    int id = request.AsMap().at("id"s).AsInt();
 
     std::stringstream stream;
     renderer_.GetSVG(tc_.GetAllRouteInfo()).Render(stream);
 
-    result["request_id"s] = id;
-    result["map"s] = json::Node(stream.str());
-    return result;
+    int id = request.AsMap().at("id"s).AsInt();
+
+    return json::Builder{}
+        .StartDict()
+        .Key("request_id"s)
+        .Value(id)
+        .Key("map"s)
+        .Value(stream.str())
+        .EndDict()
+        .Build()
+        .AsMap();
 }
 
