a260493bdb01811e8362c4cb659512e5e461deb3
Aleksei Belozerov <88431789+mixtox@users.noreply.github.com>
2022-07-03 14:48:52 +0300

    12 sprint project

--- transport-catalogue/geo.cpp
+++ transport-catalogue/geo.cpp
@@ -20,7 +20,7 @@ namespace geo {
         }
         static const double dr = M_PI / 180.;
         return acos(sin(from.lat * dr) * sin(to.lat * dr)
-                    + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
+            + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
             * EARTH_RADIUS;
     }
 
