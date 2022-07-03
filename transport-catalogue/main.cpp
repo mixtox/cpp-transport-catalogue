fc82a02fd409f7b870a9b7fbe2475a1bbe3cecda
Aleksei Belozerov <88431789+mixtox@users.noreply.github.com>
2022-07-03 15:08:03 +0300

    12 sprint project

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,18 +1,20 @@
-#include "json_reader.h"
-#include "transport_catalogue.h"
 #include "json.h"
-#include "request_handler.h"
+#include "json_reader.h"
 #include "map_renderer.h"
+#include "request_handler.h"
+#include "transport_catalogue.h"
 
-#include <fstream>
 #include <vector>
 
 
 int main() {
-    transport_catalogue::TransportCatalogue trans_cat_;
-    renderer::MapRenderer mr;
 
-    json_reader::JsonReader input(std::cin, trans_cat_, mr);
+    transport_catalogue::TransportCatalogue tc;
+    renderer::MapRenderer mr;
+    transport_router::TransportRouter tr(tc);
+
+    json_reader::JsonReader input(std::cin, tc, mr, tr);
+
     input.GetInfo(std::cout);
 
 }
