1c89d54eee51694c6859b2a6e39349e8c1f51520
Aleksei Belozerov <88431789+mixtox@users.noreply.github.com>
2022-07-03 15:11:54 +0300

    12 sprint project

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "transport_catalogue.h"
+#include "transport_router.h"
 #include "map_renderer.h"
 #include "json.h"
 #include "json_builder.h"
@@ -9,16 +10,18 @@
 class RequestHandler {
 public:
 
-    RequestHandler(const transport_catalogue::TransportCatalogue& tc, const renderer::MapRenderer& renderer);
+    RequestHandler(const transport_catalogue::TransportCatalogue &tc, const renderer::MapRenderer &renderer,
+                   const transport_router::TransportRouter& router);
 
     svg::Document RenderMap() const;
 
-    json::Dict GetRoute(const json::Node& request);
+    json::Dict GetBus(const json::Node& request);
     json::Dict GetStop(const json::Node& request);
     json::Dict GetMap(const json::Node& request);
+    json::Dict GetRoute(const json::Node& request);
 
 private:
     const transport_catalogue::TransportCatalogue& tc_;
     const renderer::MapRenderer& renderer_;
-
+    const transport_router::TransportRouter& tr_;
 };
