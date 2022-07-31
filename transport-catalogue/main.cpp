994afa95b3db1f60fa100d63bf2e463e8a74e8ea
Aleksei Belozerov <88431789+mixtox@users.noreply.github.com>
2022-07-31 23:48:58 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,20 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string_view>
+
+#include <transport_catalogue.pb.h>
+
 #include "json.h"
 #include "json_reader.h"
 #include "map_renderer.h"
-#include "request_handler.h"
-#include "transport_catalogue.h"
-
-#include <vector>
+#include "transport_router.h"
+#include "serialization.h"
 
 
-int main() {
+using namespace std::literals;
+
+void PrintUsage(std::ostream& stream = std::cerr) {
+    stream << "Usage: transport_catalogue [make_base|process_requests]\n"sv;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        PrintUsage();
+        return 1;
+    }
 
     transport_catalogue::TransportCatalogue tc;
     renderer::MapRenderer mr;
     transport_router::TransportRouter tr(tc);
+    json_reader::JsonReader input(tc, mr, tr);
+    serialization::Serialization sr(tc, input, mr, tr);
+    
+    const std::string_view mode(argv[1]);
 
-    json_reader::JsonReader input(std::cin, tc, mr, tr);
+    if (mode == "make_base"sv) {
 
-    input.GetInfo(std::cout);
+        input.ReadInfo(std::cin);
+        sr.SaveDataBase();
 
+    } else if (mode == "process_requests"sv) {
+
+        input.ReadRequest(std::cin);
+        sr.LoadDataBase();
+        input.GetResult(std::cout);
+
+    } else {
+        PrintUsage();
+        return 1;
+    }
 }
