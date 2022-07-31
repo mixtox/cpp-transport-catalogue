a16fbaedb794f8ccdbd810558fade6e6daf4c39f
Aleksei Belozerov <88431789+mixtox@users.noreply.github.com>
2022-07-31 23:52:50 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -10,7 +10,6 @@
 #include <set>
 #include <deque>
 #include <functional>
-#include <execution>
 #include <iterator>
 #include <optional>
 #include <map>
@@ -19,10 +18,10 @@ namespace transport_catalogue {
 
     struct Buses_Info {
         std::string_view route_name;
-        int stop_count = 0;
-        int unique_stop = 0;
-        double route_length = 0.0;
-        double curvature = 0.0;
+        int stop_count;
+        int unique_stop;
+        double route_length;
+        double curvature;
     };
 
     namespace detail {
@@ -43,25 +42,30 @@ namespace transport_catalogue {
         domain::StopPtr FindStop(std::string_view stop_name) const;
         domain::BusPtr FindBus(std::string_view bus_name) const;
 
-        void AddDistance(domain::StopPtr first_stop, domain::StopPtr second_stop, double distance);
+        void SetDistanceStops(domain::StopPtr first_stop, domain::StopPtr second_stop, double distance);
 
-        const std::optional<Buses_Info> GetRouteInfo(std::string input) const;
-        const std::unordered_set<domain::BusPtr>* GetBusesForStop(std::string_view input) const;
+        const std::optional<Buses_Info> GetBusInfo(std::string input) const;
+        const std::unordered_set<domain::BusPtr>* GetStopInfo(std::string_view input) const;
         size_t GetDistance(domain::StopPtr first_stop, domain::StopPtr second_stop) const;
 
-        const std::map<std::string_view, domain::BusPtr> GetAllRouteInfo() const;
+        const std::map<std::string_view, domain::BusPtr> GetAllBusesInfo() const;
         const std::unordered_map<std::string_view, domain::StopPtr> GetAllStop() const;
+
+        const std::deque<domain::Stop> GetStopBase() const;
+        const std::deque<domain::Bus> GetBusBase() const;
+        const std::unordered_map<domain::StopPtr, std::unordered_set<domain::BusPtr>> GetBusesInStopBase() const;
+        const std::unordered_map<std::pair<domain::StopPtr, domain::StopPtr>, double, detail::DistanceHasher> GetDistanceBase() const;
 
     private:
 
         std::deque<domain::Stop> stops_;
         std::deque<domain::Bus> bus_;
         
-        std::unordered_map<std::string_view, domain::BusPtr> busname_to_bus_;
-        std::unordered_map<std::string_view, domain::StopPtr> stopname_to_stop_;
+        std::unordered_map<std::string_view, domain::BusPtr> bus_binding_;
+        std::unordered_map<std::string_view, domain::StopPtr> stop_binding_;
         
         std::unordered_map<domain::StopPtr, std::unordered_set<domain::BusPtr>> buses_in_stop_;
-        std::unordered_map<std::pair<domain::StopPtr, domain::StopPtr>, double, domain::DistanceHasher> distances_map_;
+        std::unordered_map<std::pair<domain::StopPtr, domain::StopPtr>, double, detail::DistanceHasher> distance_between_stop_;
 
     };
 }
