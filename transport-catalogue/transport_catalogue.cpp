77c5eead9d7cc4261d0e38750d4dc63b7def965d
Aleksei Belozerov <88431789+mixtox@users.noreply.github.com>
2022-07-31 23:52:31 +0300

    Update transport_catalogue.cpp

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -4,13 +4,13 @@ namespace transport_catalogue {
 
     void TransportCatalogue::AddStop(std::string_view stop_name, geo::Coordinates coordinates) {
         const auto &stop = stops_.emplace_back(domain::Stop{std::string(stop_name), coordinates});
-        stopname_to_stop_.insert({stop.name, &stop});
+        stop_binding_.insert({stop.name, &stop});
         buses_in_stop_[&stop];
     }
 
     domain::StopPtr TransportCatalogue::FindStop(std::string_view stop_name) const{
-        const auto it = stopname_to_stop_.find(stop_name);
-        if (it == stopname_to_stop_.end()) {
+        const auto it = stop_binding_.find(stop_name);
+        if (it == stop_binding_.end()) {
             return nullptr;
         } else {
             return it->second;
@@ -22,7 +22,7 @@ namespace transport_catalogue {
         route_stops.reserve(stops.size());
 
         auto &route = bus_.emplace_back(domain::Bus{std::string(bus_name), route_stops, is_circle});
-        busname_to_bus_.insert({route.bus_name, &route});
+        bus_binding_.insert({route.bus_name, &route});
         for (const auto &stop: stops) {
             const auto *stop_find = FindStop(stop);
             route.bus_stops.push_back(stop_find);
@@ -31,15 +31,15 @@ namespace transport_catalogue {
     }
 
     domain::BusPtr TransportCatalogue::FindBus(std::string_view bus_name) const {
-        const auto it = busname_to_bus_.find(bus_name);
-        if (it == busname_to_bus_.end()) {
+        const auto it = bus_binding_.find(bus_name);
+        if (it == bus_binding_.end()) {
             return nullptr;
         } else {
             return it->second;
         }
     }
 
-    const std::optional<Buses_Info> TransportCatalogue::GetRouteInfo(std::string input) const {
+    const std::optional<Buses_Info> TransportCatalogue::GetBusInfo(std::string input) const {
         Buses_Info result;
         const auto *buses_info = FindBus(input);
         if (buses_info == nullptr) {
@@ -67,13 +67,13 @@ namespace transport_catalogue {
         result.route_name = buses_info->bus_name;
         result.stop_count = static_cast<int>(stops_in_route.size());
         result.unique_stop = static_cast<int>(unique_stop.size());
-        result.route_length = double(route_length_true);
+        result.route_length = route_length_true;
         result.curvature = route_length_true / route_length;
 
         return result;
     }
 
-    const std::unordered_set<domain::BusPtr>* TransportCatalogue::GetBusesForStop(std::string_view input) const{
+    const std::unordered_set<domain::BusPtr>* TransportCatalogue::GetStopInfo(std::string_view input) const{
         if (FindStop(input)) {
             return &buses_in_stop_.at(FindStop(input));
         } else {
@@ -81,20 +81,20 @@ namespace transport_catalogue {
         }
     }
 
-    void TransportCatalogue::AddDistance(domain::StopPtr first_stop, domain::StopPtr second_stop, double distance) {
+    void TransportCatalogue::SetDistanceStops(domain::StopPtr first_stop, domain::StopPtr second_stop, double distance) {
         std::pair<domain::StopPtr, domain::StopPtr> stop_pair = std::make_pair(first_stop, second_stop);
-        distances_map_[stop_pair] = distance;
+        distance_between_stop_[stop_pair] = distance;
     }
 
     size_t TransportCatalogue::GetDistance(domain::StopPtr first_stop, domain::StopPtr second_stop) const{
         auto pair_stop = std::make_pair(first_stop, second_stop);
-        if (distances_map_.find(pair_stop) != distances_map_.end()) {
-            return size_t(distances_map_.at(pair_stop));
+        if (distance_between_stop_.find(pair_stop) != distance_between_stop_.end()) {
+            return distance_between_stop_.at(pair_stop);
         }
         return GetDistance(second_stop, first_stop);
     }
 
-    const std::map<std::string_view, domain::BusPtr> TransportCatalogue::GetAllRouteInfo() const {
+    const std::map<std::string_view, domain::BusPtr> TransportCatalogue::GetAllBusesInfo() const {
         std::map<std::string_view, domain::BusPtr> result;
         for (const auto& bus : bus_) {
             result[bus.bus_name] = &bus;
@@ -103,7 +103,28 @@ namespace transport_catalogue {
     }
 
     const std::unordered_map<std::string_view, domain::StopPtr> TransportCatalogue::GetAllStop() const {
-        return stopname_to_stop_;
+        return stop_binding_;
     }
 
+    const std::deque<domain::Stop> TransportCatalogue::GetStopBase() const {
+        return stops_;
+    }
+
+    const std::deque<domain::Bus> TransportCatalogue::GetBusBase() const {
+        return bus_;
+    }
+
+    const std::unordered_map<domain::StopPtr, std::unordered_set<domain::BusPtr>>
+    TransportCatalogue::GetBusesInStopBase() const {
+        return buses_in_stop_;
+    }
+
+    const std::unordered_map<std::pair<domain::StopPtr, domain::StopPtr>, double, detail::DistanceHasher>
+    TransportCatalogue::GetDistanceBase() const {
+        return distance_between_stop_;
+    }
+
+    size_t detail::DistanceHasher::operator()(const std::pair<domain::StopPtr, domain::StopPtr> stops_pair) const {
+        return hasher(stops_pair.first) + hasher(stops_pair.second) * 37;
+    }
 }
