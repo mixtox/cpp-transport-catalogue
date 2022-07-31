b1ef0e44da7a104f2749fb91ae291713253ef4c6
Aleksei Belozerov <88431789+mixtox@users.noreply.github.com>
2022-07-31 23:43:20 +0300

    Update domain.cpp

--- transport-catalogue/domain.cpp
+++ transport-catalogue/domain.cpp
@@ -1,10 +1,1 @@
-#include "domain.h"
 
-namespace domain 
-{
-
-    size_t DistanceHasher::operator()(const std::pair<domain::StopPtr, domain::StopPtr> stops_pair) const {
-        return hasher(stops_pair.first) + hasher(stops_pair.second) * 37;
-    }
-
-}
