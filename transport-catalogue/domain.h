e1cd34a0c268ab2e7c029719e4ea936c4d9c2d74
Aleksei Belozerov <88431789+mixtox@users.noreply.github.com>
2022-07-31 23:43:40 +0300

    Update domain.h

--- transport-catalogue/domain.h
+++ transport-catalogue/domain.h
@@ -12,8 +12,6 @@
 namespace domain {
 
     struct Stop {
-        Stop() = default;
-
         std::string name;
         geo::Coordinates coordinates;
     };
@@ -21,22 +19,13 @@ namespace domain {
     using StopPtr = const Stop*;
 
     struct Bus {
-        Bus() = default;
-
-        std::string bus_number;
+        std::string bus_name;
         std::vector<StopPtr> bus_stops;
         bool is_circle;
     };
 
     using BusPtr = const Bus*;
 
-    struct DistanceHasher {
-       size_t operator()(const std::pair<domain::StopPtr, domain::StopPtr> stops_pair) const;
-
-    private:
-            std::hash<const void*> hasher;
-    };
-    
     struct RouteSettings {
         double bus_velocity = 1.0;
         int bus_wait_time = 1;
